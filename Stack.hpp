4a33244ab919eed556b689f7acba621fe00794ee
S.Azuma <61907831+azm-creater@users.noreply.github.com>
2020-03-17 08:26:39 -0400

    Add files via upload

---
+++ Stack.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    char data;
+    struct Node *next;
+};
+
+class Stack {
+    struct Node* top;
+
+    public:
+    Stack();
+    void push(char value);
+    void pop();
+};
+
+Stack::Stack() {
+    top == NULL;
+}
+
+void Stack::push(char value) {
+    struct Node* p = new struct Node;
+    p->data = value;
+    p->next = top;
+    top = p;
+}
+
+void Stack::pop() {
+    if( top == NULL ) cout << "Stack Underflow" << endl;
+    else {
+        cout << "The popped element is " << top->data << endl;
+        top = top->next;
+    }
+}
